3b7eba18a7ff2bbb0a75c5e04ce14d82ce3f9aef
AAAdzim <adzim.al.islami@gmail.com>
2023-06-09 19:15:43 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '34.101.227.234'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'wander3306wisely'
 app.config['MYSQL_DB'] = 'wander_wisely'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
